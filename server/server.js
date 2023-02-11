a503d6b4518ea59739d4716cff4d6d11169feb44
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2023-02-11 08:39:17 +0530

    edited url

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ const openai = new OpenAIApi(configuration);
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.get('/', async (req, res) => {
     res.status(200).send({
